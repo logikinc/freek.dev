66e8f241a5554b40909e8b8ac736d5c95f06892a
freekmurze <freekmurze@users.noreply.github.com>
2020-12-18 07:53:33 +0000

    Fix styling

--- tests/Feature/Controllers/CreateLinkControllerTest.php
+++ tests/Feature/Controllers/CreateLinkControllerTest.php
@@ -2,14 +2,8 @@
 
 namespace Tests\Feature\Controllers;
 
-use App\Http\Controllers\Links\CreateLinkController;
-use App\Mail\LinkSumittedMail;
-use App\Models\Link;
-use App\Models\User;
-use Illuminate\Support\Facades\Mail;
 use Tests\TestCase;
 
 class CreateLinkControllerTest extends TestCase
 {
-
 }
